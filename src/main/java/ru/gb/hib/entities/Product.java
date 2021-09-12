016296d2d4657df84eaadbfcce86d5630962ce3b
GlebKlimakhin <71899242+GlebKlimakhin@users.noreply.github.com>
2021-09-12 04:40:34 +0300

    Add files via upload

---
+++ src/main/java/ru/gb/hib/entities/Product.java
@@ -0,0 +1,35 @@
+package ru.gb.hib.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private int price;
+
+    public Product(String title, int price) {
+        this.title = title;
+        this.price = price;
+    }
+
+}

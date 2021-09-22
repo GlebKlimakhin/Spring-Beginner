5ef10fa4a400c628383e47bd665fd639dc6709e3
GlebKlimakhin <71899242+GlebKlimakhin@users.noreply.github.com>
2021-09-22 05:36:10 +0300

    Add files via upload

---
+++ src/main/java/com/gb/data/springdata/entities/Product.java
@@ -0,0 +1,29 @@
+package com.gb.data.springdata.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private int price;
+
+}
